2a90874ab10d71f168223db5d89838fda882fe5a
Ahmed Raza <ahmedraza6100@gmail.com>
2023-06-26 03:07:35 +0500

    Done with the assignment

---
+++ functions.mjs
@@ -0,0 +1,16 @@
+function checkEvenOrOdd(e) {
+    e.preventDefault();
+    let number = document.getElementById("numberInput").value;
+    let resultElement = document.querySelector(".result");
+
+    if (number.length == 0) {
+        resultElement.innerHTML = "Enter Valid Number";
+    }
+    else if (number % 2 === 0) {
+        resultElement.innerHTML = number + " is even.";
+    } else {
+        resultElement.innerHTML = number + " is odd.";
+    }
+}
+
+document.addEventListener("submit", checkEvenOrOdd);
